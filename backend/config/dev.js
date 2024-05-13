0cf86210dedd7e43954c7053d543bc09a6230582
Abdul Basit <104556643+proabdulbasit@users.noreply.github.com>
2024-05-13 18:24:03 +0500

    Update dev.js

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://barlevi_atias:l1uNlfhIKQ1bN6bV@stays.lnm3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://barlevi_atias:l1uNlfhIKQ1bN6bV@stays.lnm3w.mongodb.net/?retryWrites=true&w=majority&appName=stays',
 }
